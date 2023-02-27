e9abb4bfeaf6e7d8e9cbcbb619e399e346bf568e
IguchiShunpei <k021g1129@g.neec.ac.jp>
2023-02-27 10:31:11 +0900

    フィルターの作成など

--- Object3d.h
+++ Object3d.h
@@ -7,12 +7,10 @@
 #include <d3dx12.h>
 #include "Model.h"
 
-/// <summary>
 /// 3Dオブジェクト
-/// </summary>
 class Object3d
 {
-private: // エイリアス
+private: 
 	// Microsoft::WRL::を省略
 	template <class T> using ComPtr = Microsoft::WRL::ComPtr<T>;
 	// DirectX::を省略
@@ -38,59 +36,32 @@ private: // 定数
 	static const int vertexCount = planeCount * 3;		// 頂点数
 
 public: // 静的メンバ関数
-	/// <summary>
+
 	/// 静的初期化
-	/// </summary>
-	/// <param name="device">デバイス</param>
-	/// <param name="window_width">画面幅</param>
-	/// <param name="window_height">画面高さ</param>
 	static void StaticInitialize(ID3D12Device* device, int window_width, int window_height);
 
-	/// <summary>
 	/// 描画前処理
-	/// </summary>
-	/// <param name="cmdList">描画コマンドリスト</param>
 	static void PreDraw(ID3D12GraphicsCommandList* cmdList);
 
-	/// <summary>
 	/// 描画後処理
-	/// </summary>
 	static void PostDraw();
 
-	/// <summary>
 	/// 3Dオブジェクト生成
-	/// </summary>
-	/// <returns></returns>
 	static Object3d* Create();
 
-	/// <summary>
 	/// 視点座標の取得
-	/// </summary>
-	/// <returns>座標</returns>
 	static const XMFLOAT3& GetEye() { return eye; }
 
-	/// <summary>
 	/// 視点座標の設定
-	/// </summary>
-	/// <param name="position">座標</param>
 	static void SetEye(XMFLOAT3 eye);
 
-	/// <summary>
 	/// 注視点座標の取得
-	/// </summary>
-	/// <returns>座標</returns>
 	static const XMFLOAT3& GetTarget() { return target; }
 
-	/// <summary>
 	/// 注視点座標の設定
-	/// </summary>
-	/// <param name="position">座標</param>
 	static void SetTarget(XMFLOAT3 target);
 
-	/// <summary>
 	/// ベクトルによる移動
-	/// </summary>
-	/// <param name="move">移動量</param>
 	static void CameraMoveVector(XMFLOAT3 move);
 
 private: // 静的メンバ変数
@@ -115,46 +86,27 @@ private: // 静的メンバ変数
 
 private:// 静的メンバ関数
 
-	/// <summary>
 	/// カメラ初期化
-	/// </summary>
-	/// <param name="window_width">画面横幅</param>
-	/// <param name="window_height">画面縦幅</param>
 	static void InitializeCamera(int window_width, int window_height);
 
-	/// <summary>
 	/// グラフィックパイプライン生成
-	/// </summary>
-	/// <returns>成否</returns>
 	static void InitializeGraphicsPipeline();
 
-	/// <summary>
 	/// ビュー行列を更新
-	/// </summary>
 	static void UpdateViewMatrix();
 
 public: // メンバ関数
 	bool Initialize();
-	/// <summary>
 	/// 毎フレーム処理
-	/// </summary>
 	void Update();
 
-	/// <summary>
 	/// 描画
-	/// </summary>
 	void Draw();
 
-	/// <summary>
 	/// 座標の取得
-	/// </summary>
-	/// <returns>座標</returns>
 	const XMFLOAT3& GetPosition() const { return position; }
 
-	/// <summary>
 	/// 座標の設定
-	/// </summary>
-	/// <param name="position">座標</param>
 	void SetPosition(const XMFLOAT3& position) { this->position = position; }
 
 	//モデルsetter
